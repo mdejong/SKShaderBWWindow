8d8f18f2a51f51b83cc8a2267ef6c00351e2bac1
Mo DeJong <mo@modejong.com>
2016-04-11 19:19:08 -0700

    initial checkin of generated project and 2 nodes

--- SKShaderBWWindow/GameScene.m
+++ SKShaderBWWindow/GameScene.m
@@ -11,7 +11,54 @@
 @implementation GameScene
 
 -(void)didMoveToView:(SKView *)view {
-    /* Setup your scene here */
+  // Create a node with a large rainbow background
+  
+  SKSpriteNode *background = [SKSpriteNode spriteNodeWithImageNamed:@"Rainbow.jpg"];
+  background.position = CGPointMake(self.scene.size.width / 2, self.scene.size.height / 2);
+  background.size = self.scene.size;
+  
+  background.zPosition = 0;
+  
+  [self addChild:background];
+
+  NSLog(@"render background as %x x %d", (int)background.size.width, (int)background.size.height);
+  
+  // BW
+  
+  SKSpriteNode *bwContainer = [SKSpriteNode spriteNodeWithImageNamed:@"dummypixel.png"];
+  
+  bwContainer.position = CGPointMake(self.frame.size.width/2, self.frame.size.height/2);
+  bwContainer.size = CGSizeMake(self.frame.size.width / 8, self.frame.size.height / 8);
+  
+  //Create the shader from a shader-file
+  SKShader* shader = [SKShader shaderWithFileNamed:@"shader_bw.fsh"];
+
+  bwContainer.shader = shader;
+  
+  bwContainer.zPosition = 1;
+  
+  [self addChild:bwContainer];
+
+  
+  /*
+  
+  //Setup a container sprite for the shader that makes the movement
+  SKSpriteNode *shaderContainer_move = [SKSpriteNode spriteNodeWithImageNamed:@"dummypixel.png"];
+  shaderContainer_move.position = CGPointMake(self.frame.size.width/2, self.frame.size.height/2);
+  shaderContainer_move.size = CGSizeMake(self.frame.size.width, self.frame.size.height);
+  [self addChild:shaderContainer_move];
+  //Create the shader from a shader-file
+  SKShader* shader_move = [SKShader shaderWithFileNamed:@"shader_water_movement.fsh"];
+  //Set vairiables that are used in the shader script
+  shader_move.uniforms = @[
+                           //                        [SKUniform uniformWithName:@"size" floatVector3:GLKVector3Make(self.frame.size.width*1.5, self.frame.size.height*1.5, 0)],
+                           [SKUniform uniformWithName:@"customTexture" texture:[SKTexture textureWithImageNamed:@"sand.png"]],
+                           ];
+  //add the shader to the sprite
+  shaderContainer_move.shader = shader_move;
+
+  
+    // Setup your scene here
     SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
     
     myLabel.text = @"Hello, World!";
@@ -20,15 +67,21 @@
                                    CGRectGetMidY(self.frame));
     
     [self addChild:myLabel];
+
+   */
 }
 
+/*
+
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    /* Called when a touch begins */
+    // Called when a touch begins
     
     for (UITouch *touch in touches) {
         CGPoint location = [touch locationInNode:self];
         
         SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
+      
+      // Move window to this position?
         
         sprite.xScale = 0.5;
         sprite.yScale = 0.5;
@@ -42,8 +95,10 @@
     }
 }
 
+*/
+
 -(void)update:(CFTimeInterval)currentTime {
-    /* Called before each frame is rendered */
+    // Called before each frame is rendered
 }
 
 @end
---
+++ SKShaderBWWindow/shader_bw.fsh
@@ -0,0 +1,7 @@
+void main(void)
+{
+  //vec2 uv = v_tex_coord.xy;
+  //vec4 texColor = texture2D(customTexture,uv);
+  vec4 texColor = vec4(0.0, 0.0, 0.0, 1.0);
+  gl_FragColor = texColor;
+}
